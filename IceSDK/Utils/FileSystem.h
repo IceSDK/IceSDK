acc3d9eeb163ba23f4312cf17639b9e2be9df928
Robin A. P <me@mempler.de>
2020-09-27 01:25:23 +0000

    Speedy Strings

--- IceSDK/Utils/FileSystem.h
+++ IceSDK/Utils/FileSystem.h
@@ -7,19 +7,19 @@ namespace IceSDK
 {
     namespace FileSystem
     {
-        void MkDir(const std::string& pPath);
-        std::string JoinPath(const std::string& p1, const std::string& p2);
-        bool Exists(const std::string& pPath);
-        bool IsDirectory(const std::string& pPath);
-        std::vector<std::string> ReadDirectory(const std::string& pPath,
+        void MkDir(std::string_view pPath);
+        std::string JoinPath(std::string_view p1, std::string_view p2);
+        bool Exists(std::string_view pPath);
+        bool IsDirectory(std::string_view pPath);
+        std::vector<std::string> ReadDirectory(std::string_view pPath,
                                                bool pRecursive = false);
-        std::string ResolveFullPath(const std::string& pPath);
-        std::vector<uint8_t> ReadBinaryFile(const std::string& pPath);
-        void WriteBinaryFile(const std::string& pPath,
-                             std::vector<uint8_t> pData);
-        bool HasExtension(const std::string& pPath, const std::string& pExt);
-        std::string GetFileName(const std::string& pPath);
-        void Touch(const std::string& pPath);
-        void Delete(const std::string& pPath);
+        std::string ResolveFullPath(std::string_view pPath);
+        std::vector<uint8_t> ReadBinaryFile(std::string_view pPath);
+        void WriteBinaryFile(std::string_view pPath,
+                             const std::vector<uint8_t>& pData);
+        bool HasExtension(std::string_view pPath, std::string_view pExt);
+        std::string_view GetFileName(std::string_view pPath);
+        void Touch(std::string_view pPath);
+        void Delete(std::string_view pPath);
     }  // namespace FileSystem
 }  // namespace IceSDK
